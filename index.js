a37649b687243477e6d06479be5c9dfd3c754a88
Cruelasss <irula5198@gmail.com>
2025-10-21 10:10:22 +0700

    Buat Endpoint untuk Mengambil Data Biodata

--- index.js
+++ index.js
@@ -29,4 +29,13 @@ db.connect((err) => {
   console.log('Connection Succesfully!');
 });
 
-
+app.get('/biodata', (req, res) => {
+  db.query('SELECT * FROM mahasiswa', (err, results) => {
+    if (err) {
+        console.error('Error fetching data:', err.stack);
+        res.status(500).send('Error fetching data');
+        return;
+    }
+    res.json(results);
+  });
+});
